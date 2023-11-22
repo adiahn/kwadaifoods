1cec0f47d7fcde043fbedc4071ec5987aa09e5cd
adiahn <adnanmukhtar2321@gmail.com>
2023-11-22 12:13:25 +0100

    first deployment

--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function header() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className='text-3xl text-primary'>hello world</h1>
-      <h1 className='text-3xl text-secondry'>hello world</h1>
+      <h1 className='text-3xl text-[yellow]'>hello world</h1>
     </main>
   )
 }
