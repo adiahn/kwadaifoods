bcbcfbb56ada86db2f04c2d71af2c9e26131abf9
adiahn <adnanmukhtar2321@gmail.com>
2023-11-27 18:31:32 +0100

    New Push

--- src/app/Components/Logs/Login.jsx
+++ src/app/Components/Logs/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
           <span className='font-extrabold text-black tracking-wide text-5xl leading-normal'>Welcome to</span>
           <span className='text-2xl font-bold text-secondary'>KwadaiFoods</span>
         </h1>
-        <p>Your favori  te organic foods plug, delivered to your doorstep</p>
+        <p className='leading-normal'>Your favori  te organic foods plug, delivered to your doorstep</p>
       </div>
       <div className='flex flex-col justify-around h-2/6'>
         <h1 className='text-center text-primary tracking-wider'>
@@ -19,17 +19,17 @@ const Login = () => {
         </h1>
 
         <div className='flex justify-around'>
-          <button className='border-2 w-40 h-14 bg-primary rounded-full text-black font-bold flex items-center justify-center'>
-            <Image src={googleLogo} alt='Facebook Logo' className='h-6 w-6 mr-2' />
+          <button className='border-2 w-40 h-14 bg-primary rounded-full text-black font-bold flex text-lg items-center justify-center'>
+            <Image src={facebookLogo} alt='Facebook Logo' className='h-8 w-8 mr-2' />
             Facebook
           </button>
-          <button className=' w-36 h-14 bg-primary rounded-full text-black font-bold flex items-center justify-center'>
-            <Image src={facebookLogo} alt='Google Logo' className='h-6 w-6 mr-2' />
+          <button className=' w-36 h-14 bg-primary rounded-full text-black font-bold text-lg flex items-center justify-center'>
+            <Image src={googleLogo} alt='Google Logo' className='h-8 w-8 mr-2' />
             Google
           </button>
         </div>
         <div className='flex justify-around items-center flex-col'>
-          <button className='w-80 h-14 border-2 text-primary rounded-full bg-back'>Start with E-mail or Phone</button>
+          <button className='w-80 h-16 border-2 text-primary rounded-full  text-lg bg-back'>Start with E-mail or Phone</button>
         </div>
 
       </div>
